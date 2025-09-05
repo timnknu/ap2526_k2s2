dd01afca05f1cb5f9e2a5b6e80fc372cc8315594
Your Name <you@example.com>
2025-09-05 13:41:52 +0300

    t17_9

--- 01_decorators/t17_9.py
+++ 01_decorators/t17_9.py
@@ -1,4 +1,18 @@
+def modifier(F):
+    res_storage = {}
+    def newfunc(x):
+        if x in res_storage:
+            print('Computation optimized!', x)
+            return res_storage[x]
+        else:
+            print('New computation', x)
+            y = F(x)
+            res_storage[x] = y
+            return y
+    return newfunc
 
+
+@modifier  # Idential to: fib = modifier(fib)
 def fib(n):
     if n == 1:
         return 1
@@ -6,17 +20,6 @@ def fib(n):
         return 1
     return fib(n-1) + fib(n-2)
 
-# for i in range(3, 10):
-#     print(i, fib(i))
-
-def modifier(F):
-    def newfunc(x):
-        return F(x)+1
-    return newfunc
-
-@modifier
-def square(x):
-    return x**2
-print(square(20))
-#G = modifier(square)
-#print(G(10))
+print(fib(4))
+print('----------')
+print(fib(5))
