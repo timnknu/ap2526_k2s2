aa8c732e389727fb499edc7efe517c7a36e121ca
Your Name <you@example.com>
2025-09-12 12:37:28 +0300

    upd

--- 02_metaprogr/deco_class.py
+++ 02_metaprogr/deco_class.py
@@ -1,9 +1,16 @@
+def strmagic_injector(cls):
+    def pretty_formatter(self):
+        return f"My very beautifu object has a={self.a}"
+    cls.__str__ = pretty_formatter
+    #setattr(cls, '__str__', pretty_formatter)
+    return cls
+
+@strmagic_injector
 class A:
     def __init__(self):
         self.a = 12
         self.b = 25.01
-    def __str__(self):
-        return f"My very beautifu object has a={self.a}"
+
 
 obj = A()
 print(obj)
