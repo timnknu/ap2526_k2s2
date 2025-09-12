8ec963a39734b70bc587766915f8537e2c696e26
Your Name <you@example.com>
2025-09-12 13:22:15 +0300

    the 'logger/debugger' metaclass

--- 02_metaprogr/metaclass1_demo.py
+++ 02_metaprogr/metaclass1_demo.py
@@ -1,13 +1,31 @@
-# class A:
-#     def f1(self):
-#         print(123, self.xarg)
-#     def __init__(self, x):
-#         self.xarg = x
+class MyMetaClass(type):
+    def __new__(*args, **kwargs):
+        print('Metaclass __new__', args, kwargs)
+        res = type.__new__(*args, **kwargs)
+        print('-->', res)
+        return res
+
+    def __init__(*args, **kwargs):
+        print('Metaclass __init__', args, kwargs)
+        res = type.__init__(*args, **kwargs)
+        print('-->', res)
+        return res
+
+    def __call__(*args, **kwargs):
+        print('Metaclass __call__', args, kwargs)
+        res = type.__call__(*args, **kwargs)
+        print('-->', res)
+        return res
+
+class A(metaclass=MyMetaClass): # за замовчуванням (якщо нічого не вказувати) тут metaclass=type
+    def square(self, x):
+        print('This is square()')
+        return x**2
+    def __init__(self, x):
+        self.xarg = x
 
 
-C = type('MyClass', tuple(), {
-    'square': lambda self, x: x**2
-})
-obj = object.__new__(C)
+obj = object.__new__(A)
+obj.__init__('hello')
 print(obj.square(12))
 print(type(obj))
