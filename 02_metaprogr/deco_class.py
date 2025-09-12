bdbf0020afbeeb09f6af2754eb4fb94e17a3c0d0
Your Name <you@example.com>
2025-09-12 12:43:42 +0300

    class attr printer

--- 02_metaprogr/deco_class.py
+++ 02_metaprogr/deco_class.py
@@ -1,14 +1,19 @@
-def strmagic_injector(cls):
-    def pretty_formatter(self):
-        return f"My very beautifu object has a={self.a}"
-    cls.__str__ = pretty_formatter
-    #setattr(cls, '__str__', pretty_formatter)
-    return cls
+def strmagic_injector_universal(*args):
+    def strmagic_injector(cls):
+        def pretty_formatter(self):
+            s = f"My very beautifu object has:"
+            for a in args:
+                s += f"{a} = {getattr(self, a)}, "
+            return s
+        cls.__str__ = pretty_formatter
+        #setattr(cls, '__str__', pretty_formatter)
+        return cls
+    return strmagic_injector
 
-@strmagic_injector
+@strmagic_injector_universal('par', 'b')
 class A:
     def __init__(self):
-        self.a = 12
+        self.par = 12
         self.b = 25.01
 
 
