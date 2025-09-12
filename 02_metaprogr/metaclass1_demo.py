e05fae23bf35f646ca8fb38ced633fe2b0103cfc
Your Name <you@example.com>
2025-09-12 13:08:39 +0300

    creating a class explicitly, without using 'class ... :'

--- 02_metaprogr/metaclass1_demo.py
+++ 02_metaprogr/metaclass1_demo.py
@@ -1,13 +1,13 @@
-class A:
-    def f1(self):
-        print(123, self.xarg)
-    def __init__(self, x):
-        self.xarg = x
+# class A:
+#     def f1(self):
+#         print(123, self.xarg)
+#     def __init__(self, x):
+#         self.xarg = x
 
-B2 = A
-del A
-obj = B2('hello')
-obj.f1()
+
+C = type('MyClass', tuple(), {
+    'square': lambda self, x: x**2
+})
+obj = object.__new__(C)
+print(obj.square(12))
 print(type(obj))
-
-second_object = A('kkk')
