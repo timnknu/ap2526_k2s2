88a0ac71850a1d307044fa63a05431a604f20acc
Your Name <you@example.com>
2025-11-14 13:06:08 +0200

    v1

--- 04_OS/mkfiles_for_22_1_2_3.py
+++ 04_OS/mkfiles_for_22_1_2_3.py
@@ -18,20 +18,25 @@ def get_random_file_name(n_max_file_len = 20):
         result += allowed_chars[j]
     return result + '.txt'
 
-for i in range(10):
-    print(get_random_file_name())
+# for i in range(10):
+#     print(get_random_file_name())
 
 def make_sample_directory(folder_name, r_random_files):
     if os.path.exists(folder_name):
         print('already exist', folder_name)
     else:
         os.mkdir(folder_name)
+
     for i in range(r_random_files):
-        pass
-        #with open(file_name
+        fnm = get_random_file_name()
+        full_fnm = os.path.join(folder_name, fnm)
+        with open(full_fnm, 'w') as fo:
+            print(f'This is my file {fnm}', file=fo)
+            print('And it works great!', file=fo)
 
 
-
-
-#make_sample_directory('folder1')
+#make_sample_directory('folder1', r_random_files=2)
+#print()
+for fnm in os.listdir('folder1'):
+    print(fnm, os.path.isfile(os.path.join('folder1', fnm)))
 
