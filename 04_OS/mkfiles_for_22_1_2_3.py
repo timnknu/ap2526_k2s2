2f90c232490c67c3965caf61b0ee7b6f0f51e45e
Your Name <you@example.com>
2025-11-14 13:28:52 +0200

    v2

--- 04_OS/mkfiles_for_22_1_2_3.py
+++ 04_OS/mkfiles_for_22_1_2_3.py
@@ -1,6 +1,7 @@
 import random
 import os
-
+import time
+import shutil
 
 def get_random_file_name(n_max_file_len = 20):
     allowed_chars = ''
@@ -22,21 +23,38 @@ def get_random_file_name(n_max_file_len = 20):
 #     print(get_random_file_name())
 
 def make_sample_directory(folder_name, r_random_files):
+    # створюємо нову папку, але лише в тому разі, якщо її ще немає
     if os.path.exists(folder_name):
         print('already exist', folder_name)
     else:
         os.mkdir(folder_name)
 
+    # видаляємо із папки всі файли, які в ній були раніше (але саме файли, а не вкладені папки)
+    for fnm in os.listdir(folder_name):
+        full_name = os.path.join(folder_name, fnm)
+        if os.path.isfile(full_name):
+            os.remove(full_name)
+
+    # наповнюємо папку новими файлами із випадково згенерованими іменами
     for i in range(r_random_files):
         fnm = get_random_file_name()
         full_fnm = os.path.join(folder_name, fnm)
         with open(full_fnm, 'w') as fo:
             print(f'This is my file {fnm}', file=fo)
             print('And it works great!', file=fo)
+        time.sleep(1.2)
+#
 
-
-#make_sample_directory('folder1', r_random_files=2)
+# make_sample_directory('folder1', r_random_files=4)
+# make_sample_directory('folder2', r_random_files=4)
 #print()
-for fnm in os.listdir('folder1'):
-    print(fnm, os.path.isfile(os.path.join('folder1', fnm)))
 
+# Додаємо кілька файлів однакового вмісту та атриибутів (зокрема -- датою створення) у обидві папки
+for i in range(1, 3):
+    full_name = os.path.join('folder1', f'same_file_{i}.txt')
+    with open(full_name, 'w') as fo:
+        print(f'This file is the same in both folders, #{i}', file=fo)
+
+    #time.sleep(2)
+    dst = os.path.join('folder2', f'same_file_{i}.txt')
+    shutil.copyfile(full_name, dst)
