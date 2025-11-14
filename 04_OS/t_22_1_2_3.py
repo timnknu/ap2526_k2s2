46613b4411af76a246a7e42074ea25ba72482d45
Your Name <you@example.com>
2025-11-14 13:41:44 +0200

    v1

--- 04_OS/t_22_1_2_3.py
+++ 04_OS/t_22_1_2_3.py
@@ -1,9 +1,14 @@
 import os
+import datetime
 
 #
-print("this is my great program")
-#os.chdir('/home/tniko')
-print("this is my great program")
+print(os.listdir('folder1'))
+print(os.listdir('folder2'))
 
-print( os.getcwd())
-print(os.listdir(path='.'))
+folder_name = 'folder1'
+for fnm in os.listdir(folder_name):
+    full_name = os.path.join(folder_name, fnm)
+    if os.path.isfile(full_name):
+        timestamt = os.path.getctime(full_name)
+        d = datetime.datetime.fromtimestamp(timestamt)
+        print(fnm, os.path.getsize(full_name), d)
