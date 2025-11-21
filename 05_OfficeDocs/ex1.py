33b3f0b816b41f38e39f7a6fb833ffe1fb5e3741
Your Name <you@example.com>
2025-11-21 13:50:15 +0200

    v3

--- 05_OfficeDocs/ex1.py
+++ 05_OfficeDocs/ex1.py
@@ -1,3 +1,4 @@
+
 import openpyxl
 
 wb = openpyxl.load_workbook('sample_src_table.xlsx')
@@ -7,5 +8,7 @@ wsheet = wb[wb.sheetnames[0]]
 for i in range(3, wsheet.max_row+1):
     x = wsheet.cell(row=i, column=1).value
     wsheet.cell(row=i, column=2).value = x**2
+    wsheet.cell(row=i, column=2).fill = openpyxl.styles.PatternFill(start_color='FF00FF',
+                end_color='0000FF', fill_type="solid")
 
 wb.save('sample_edited1.xlsx')
