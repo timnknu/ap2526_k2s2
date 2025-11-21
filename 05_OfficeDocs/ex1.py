5c89ffd14ea384b086c162cd1d9ae7ed4ac2d2c4
Your Name <you@example.com>
2025-11-21 13:53:36 +0200

    v4

--- 05_OfficeDocs/ex1.py
+++ 05_OfficeDocs/ex1.py
@@ -8,7 +8,12 @@ wsheet = wb[wb.sheetnames[0]]
 for i in range(3, wsheet.max_row+1):
     x = wsheet.cell(row=i, column=1).value
     wsheet.cell(row=i, column=2).value = x**2
-    wsheet.cell(row=i, column=2).fill = openpyxl.styles.PatternFill(start_color='FF00FF',
-                end_color='0000FF', fill_type="solid")
+    xmin = 5
+    xmax = 30
+    shade = round( 255 - (x - xmin)/(xmax - xmin) * 255 )
+    fill_color = 'FFFF' + f'{shade:02X}'
+    print(fill_color)
+    wsheet.cell(row=i, column=1).fill = openpyxl.styles.PatternFill(start_color=fill_color,
+                end_color=fill_color, fill_type="solid")
 
 wb.save('sample_edited1.xlsx')
