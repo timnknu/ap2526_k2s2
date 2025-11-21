434c13e13ef9083e3e9d203ab91d165f7722344c
Your Name <you@example.com>
2025-11-21 13:47:56 +0200

    v2

--- 05_OfficeDocs/ex1.py
+++ 05_OfficeDocs/ex1.py
@@ -1,11 +1,11 @@
 import openpyxl
 
 wb = openpyxl.load_workbook('sample_src_table.xlsx')
-print(wb.sheetnames)
+#print(wb.sheetnames)
 wsheet = wb[wb.sheetnames[0]]
 
-# for i in range(1, 20):
-#     print(wsheet.cell(row=i, column=1).value)
-for r in wsheet.iter_rows():
-    print(r)
-print(wsheet.max_row, wsheet.max_column)
+for i in range(3, wsheet.max_row+1):
+    x = wsheet.cell(row=i, column=1).value
+    wsheet.cell(row=i, column=2).value = x**2
+
+wb.save('sample_edited1.xlsx')
