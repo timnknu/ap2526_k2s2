06579405c7ff1cef9a030b3d0f6e09bf3d582dea
Your Name <you@example.com>
2025-11-21 14:14:35 +0200

    v5

--- 05_OfficeDocs/ex1.py
+++ 05_OfficeDocs/ex1.py
@@ -5,11 +5,19 @@ wb = openpyxl.load_workbook('sample_src_table.xlsx')
 #print(wb.sheetnames)
 wsheet = wb[wb.sheetnames[0]]
 
+xvals = []
+for i in range(3, wsheet.max_row+1):
+    x = wsheet.cell(row=i, column=1).value
+    xvals.append(x)
+print(xvals)
+
+xmin = min(xvals)
+xmax = max(xvals)
+print(f"мінімальне: {xmin}, максимальне: {xmax}")
+
 for i in range(3, wsheet.max_row+1):
     x = wsheet.cell(row=i, column=1).value
     wsheet.cell(row=i, column=2).value = x**2
-    xmin = 5
-    xmax = 30
     shade = round( 255 - (x - xmin)/(xmax - xmin) * 255 )
     fill_color = 'FFFF' + f'{shade:02X}'
     print(fill_color)
