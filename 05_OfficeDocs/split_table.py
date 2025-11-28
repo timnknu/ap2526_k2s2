d29484201db03c09bbaacdbebaa767dd6332b8f4
Your Name <you@example.com>
2025-11-28 13:33:38 +0200

    v2-pre-final

--- 05_OfficeDocs/split_table.py
+++ 05_OfficeDocs/split_table.py
@@ -1,15 +1,29 @@
 import docx
 
 new_doc = docx.Document()
-new_doc.add_paragraph().add_run('Here is my new small table')
 
-t = new_doc.add_table(rows = 2, cols = 5)
-#new_r = t.add_row()
-t.cell(1,1).text = 'SAMPLE'
-new_doc.save('generated_table.docx')
+n_rows_we_want = 10
+
+new_doc.add_paragraph().add_run('Here is my new small table')
 
 
 d = docx.Document('big_table.docx')
 tbl = d.tables[0]
-for row in tbl.rows:
+for J, row in enumerate(tbl.rows):
     print(row.cells[0].text)
+    if J % n_rows_we_want == 0:
+        new_doc.add_paragraph().add_run('New part')
+        t_new = new_doc.add_table(rows=0, cols=len(tbl.columns))
+        if J>0:
+            new_row = t_new.add_row()
+            for i, src_cell in enumerate(tbl.rows[0].cells):
+                new_row.cells[i].text = src_cell.text
+
+    #
+    new_row = t_new.add_row()
+    for i, src_cell in enumerate(row.cells):
+        new_row.cells[i].text = src_cell.text
+    #
+
+
+new_doc.save('generated_table.docx')
