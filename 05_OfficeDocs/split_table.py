60c8263622e57470c812887cd51dd35edf9e9240
Your Name <you@example.com>
2025-11-28 13:20:57 +0200

    v1

--- 05_OfficeDocs/split_table.py
+++ 05_OfficeDocs/split_table.py
@@ -1,5 +1,14 @@
 import docx
 
+new_doc = docx.Document()
+new_doc.add_paragraph().add_run('Here is my new small table')
+
+t = new_doc.add_table(rows = 2, cols = 5)
+#new_r = t.add_row()
+t.cell(1,1).text = 'SAMPLE'
+new_doc.save('generated_table.docx')
+
+
 d = docx.Document('big_table.docx')
 tbl = d.tables[0]
 for row in tbl.rows:
