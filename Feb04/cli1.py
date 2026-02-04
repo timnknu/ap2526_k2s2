e333c6638a3fa5b0f80f2736bef4d048da265b3c
Your Name <you@example.com>
2026-02-04 13:41:11 +0200

    v1

--- Feb04/cli1.py
+++ Feb04/cli1.py
@@ -1,4 +1,5 @@
 import socket
+import traceback
 
 HOST = 'localhost'    # Комп'ютер для з'єднання з сервером
 PORT = 15555          # Порт для з'єднання з сервером
@@ -6,15 +7,23 @@ PORT = 15555          # Порт для з'єднання з сервером
 # створити гніздо
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT)) # з'єднатися з сервером
+try:
+    userid = input("Enter you nickname:")
+    assert userid.find(',') == -1
+    towhom = input("To whom you would like to send a message?")
+    assert towhom.find(',') == -1
+    msg = input("Now enter your message")
 
+    data_to_send = f'{userid},{towhom},{msg}'
+    print('[debug] sending to server:', data_to_send)
 
-to_send = "This is a test string"
-barr = to_send.encode()
-print(barr, type(barr))
-s.sendall(barr)
+    s.sendall( data_to_send.encode() )
 
-data = s.recv(1024)     # отримати відповідь сервера
-ds = data.decode()
-print(ds)
+    data = s.recv(1024)     # отримати відповідь сервера
+    ds = data.decode()
+    print(ds)
+except:
+    print('Помилка')
+    traceback.print_exc()
 
 s.close()                   # завершити з'єднання
--- Feb04/server.py
+++ Feb04/server.py
@@ -12,6 +12,8 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 s.bind((HOST, PORT))        # зв'язати з комп'ютером та портом
 s.listen(5)                 # очікувати на з'єднання
 
+storage = {} # ключ -- для кого, значення -- [(від кого, повідомлення), ...]
+
 try:
     while True:
         print('Before accept() is called')
@@ -21,7 +23,17 @@ try:
 
         data = conn.recv(1024)  # отримати дані (рядок байтів)
         ds = data.decode()
-        print('>> from client:', ds, type(ds))
+        userdata = ds.split(',')
+        if len(userdata) < 3:
+            conn.close()
+            continue
+
+        senderid = userdata[0]
+        towhom = userdata[1]
+        msg = ','.join(userdata[2:])
+        print('>> from:', senderid)
+        print('>> to:', towhom)
+        print('>> message:', msg)
 
         resp = 'ok'
         conn.sendall( resp.encode() )
