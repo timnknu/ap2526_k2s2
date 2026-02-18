4a8ad7d5c8643021c05c950b9909523b3d3f3ea7
Your Name <you@example.com>
2026-02-18 11:19:38 +0200

    v0

--- Feb18_threads/t31_14_simplified.py
+++ Feb18_threads/t31_14_simplified.py
@@ -1,10 +1,36 @@
 import os
 import time
 
-folder_path = '/tmp'
-for fnm in os.listdir(folder_path):
-    full_file_name = os.path.join(folder_path, fnm)
-    mtime = os.path.getmtime(full_file_name)
-    fsize = os.path.getsize(full_file_name)
-    if fsize > 10000:
-        print(full_file_name, '--', time.ctime(mtime),  fsize)
+def get_dir_contents(folder_path):
+    res = {} # full_file_name -> (file_size, mtime)
+    for fnm in os.listdir(folder_path):
+        full_file_name = os.path.join(folder_path, fnm)
+        mtime = os.path.getmtime(full_file_name)
+        fsize = os.path.getsize(full_file_name)
+        res[full_file_name] = (fsize, mtime)
+    return res
+
+
+def monitor_folder_changes(folder_path = '/tmp'):
+    monitoring_interval = 2.0
+    prev_snapshot = get_dir_contents(folder_path)
+
+    while True:
+        time.sleep(monitoring_interval)
+        new_snapshot = get_dir_contents(folder_path)
+        for full_file_name, new_file_info in new_snapshot.items():
+            if full_file_name in prev_snapshot:
+                old_file_info = prev_snapshot[full_file_name]
+                if old_file_info != new_file_info:
+                    print(f'in file {full_file_name}: contents changed')
+            else:
+                print(f'NEW FILE: {full_file_name}')
+        #
+        for full_file_name, new_file_info in prev_snapshot.items():
+            if full_file_name not in new_snapshot:
+                print(f'Deleted: {full_file_name}')
+        #
+        prev_snapshot = new_snapshot
+#
+
+monitor_folder_changes('.')
