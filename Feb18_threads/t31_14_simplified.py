9853ca969ad90ed1d572ff3bb49549d100ca2738
Your Name <you@example.com>
2026-02-18 12:11:56 +0200

    v1b ( імітація помилок + обгортка потока у клас)

--- Feb18_threads/t31_14_simplified.py
+++ Feb18_threads/t31_14_simplified.py
@@ -5,8 +5,8 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-
 event_terminate = threading.Event()
+event_thread_failed = threading.Event()
 
 def get_dir_contents(folder_path):
     res = {} # full_file_name -> (file_size, mtime)
@@ -17,39 +17,55 @@ def get_dir_contents(folder_path):
         res[full_file_name] = (fsize, mtime)
     return res
 
+class MyMonitoringThread(threading.Thread):
+    def __init__(self, folder_path):
+        self._folder_path = folder_path
+        super().__init__()
 
-def monitor_folder_changes(folder_path = '/tmp'):
-    monitoring_interval = 1.0
-    prev_snapshot = get_dir_contents(folder_path)
+    def monitor_folder_changes(self):
+        folder_path = self._folder_path
+        monitoring_interval = 1.0
+        prev_snapshot = get_dir_contents(folder_path)
 
-    while True:
-        if event_terminate.wait(monitoring_interval):
-            logging.debug('Thread was informed that it needs to terminate!')
-            break
-        else:
-            logging.debug(' .wait() returned false')
+        while True:
+            if event_thread_failed.is_set():
+                break
 
-        if folder_path=='/tmp':
-            1/0
-
-        new_snapshot = get_dir_contents(folder_path)
-        for full_file_name, new_file_info in new_snapshot.items():
-            if full_file_name in prev_snapshot:
-                old_file_info = prev_snapshot[full_file_name]
-                if old_file_info != new_file_info:
-                    logging.info(f'in file {full_file_name}: contents changed')
+            if event_terminate.wait(monitoring_interval):
+                logging.debug('Thread was informed that it needs to terminate!')
+                break
             else:
-                logging.info(f'NEW FILE: {full_file_name}')
-        #
-        for full_file_name, new_file_info in prev_snapshot.items():
-            if full_file_name not in new_snapshot:
-                logging.info(f'Deleted: {full_file_name}')
-        #
-        prev_snapshot = new_snapshot
-#
+                logging.debug(' .wait() returned false')
 
-th1 = threading.Thread(target=monitor_folder_changes, args=('/tmp',))
-th2 = threading.Thread(target=monitor_folder_changes, args=('.',))
+            if folder_path=='/tmp':
+                1/0
+
+            new_snapshot = get_dir_contents(folder_path)
+            for full_file_name, new_file_info in new_snapshot.items():
+                if full_file_name in prev_snapshot:
+                    old_file_info = prev_snapshot[full_file_name]
+                    if old_file_info != new_file_info:
+                        logging.info(f'in file {full_file_name}: contents changed')
+                else:
+                    logging.info(f'NEW FILE: {full_file_name}')
+            #
+            for full_file_name, new_file_info in prev_snapshot.items():
+                if full_file_name not in new_snapshot:
+                    logging.info(f'Deleted: {full_file_name}')
+            #
+            prev_snapshot = new_snapshot
+    #
+    def run(self):
+        try:
+            self.monitor_folder_changes()
+        except:
+            print('В ПОТОЦІ СТАЛАСЯ ПОМИЛКА!!!!!!!!!!!!!')
+            event_thread_failed.set()
+
+
+th1 = MyMonitoringThread('/tmp')
+th2 = MyMonitoringThread('.')
+
 
 logging.debug('Threads created')
 
@@ -64,5 +80,8 @@ try:
 except KeyboardInterrupt:
     logging.info('Interruption via Ctrl-C detected')
     event_terminate.set()
+
+if event_thread_failed.is_set():
+    logging.info('Програма завершуєтсья аварійно, бо аварійно завершився один із її потоків, а в цій програмі не було передбачено, як дійся в такому випадку! Караул!!!')
 
 logging.info('Program is finished')
