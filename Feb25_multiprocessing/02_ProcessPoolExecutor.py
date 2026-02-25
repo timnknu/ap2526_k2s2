371204d6d1995b9e26ab6415f16d230514be6912
Your Name <you@example.com>
2026-02-25 11:42:28 +0200

    v1

--- Feb25_multiprocessing/02_ProcessPoolExecutor.py
+++ Feb25_multiprocessing/02_ProcessPoolExecutor.py
@@ -11,18 +11,25 @@ def place_points(nPoints):
         if r2 < 1:
             nInside += 1
     # nInside/nPoints == (pi * 1**2 / 4) / 1**2 = pi/4
-    return nInside
+    return (nInside, nPoints)
 
 
 if __name__ == '__main__':
     nPts = [50_000, 100_000, 500_000, 1_000_000, 5_000_000, 10_000_000, 50_000_000, 100_000_000]
 
+    all_tasks = []
     with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
-        result = executor.map(place_points, nPts)
+        for n in nPts:
+            task = executor.submit(place_points, n)
+            print('Submitting:', task, type(task))
+            all_tasks.append(task)
+    print('ALL SUBMITTED')
 
-        print(result)
-    #
-    for one_nInside, one_nPoints in zip(result, nPts):
+    for future in concurrent.futures.as_completed(all_tasks):
+        data = future.result()
+        print('DATA:', data)
+        one_nInside, one_nPoints = data
         pi_estimate = one_nInside / one_nPoints * 4
         print(one_nInside, one_nPoints, '==>', pi_estimate)
+        print()
 
