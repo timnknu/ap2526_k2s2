881b30902adcda37fee220ea8663adf23d46f5a6
Your Name <you@example.com>
2026-03-25 12:06:17 +0200

    v0

---
+++ Mar26_web/ex2.py
@@ -0,0 +1,30 @@
+import http.server
+import traceback
+
+HOST = ''                 # Комп'ютер для з'єднання
+PORT = 15555              # Порт для з'єднання
+
+
+class MyClientHandler(http.server.SimpleHTTPRequestHandler):
+    pass
+
+####################
+
+srv = http.server.ThreadingHTTPServer((HOST, PORT), MyClientHandler, bind_and_activate=False)
+srv.allow_reuse_port = True
+srv.allow_reuse_address = True
+srv.server_bind()
+srv.server_activate()
+
+print('Server is listening...')
+try:
+    srv.serve_forever()
+except KeyboardInterrupt:
+    print('Дочасне завершення програми через Ctrl+C або його аналог')
+except:
+    print('Помилка')
+    traceback.print_exc()
+
+srv.shutdown()
+
+print('Сервер завершив свою роботу!')
