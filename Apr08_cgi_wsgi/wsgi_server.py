acc36446a7987cb4b2392a1bb52324c03d2c1b54
Your Name <you@example.com>
2026-04-08 11:53:00 +0300

    v1 - WSGI POST

--- Apr08_cgi_wsgi/wsgi_server.py
+++ Apr08_cgi_wsgi/wsgi_server.py
@@ -1,6 +1,7 @@
 import wsgiref.simple_server
 import traceback
-
+import urllib.parse
+from os import environ
 
 HOST = ''                 # Комп'ютер для з'єднання
 PORT = 15555              # Порт для з'єднання
@@ -8,11 +9,56 @@ PORT = 15555              # Порт для з'єднання
 
 def f(environ, start_response):
     status = '200 OK'  # HTTP Status
-    headers = [('Content-type', 'text/html')]  # HTTP Headers
-    start_response(status, headers)
+    pth = environ['PATH_INFO']
+    if pth == "/":
+        headers = [('Content-type', 'text/html')]  # HTTP Headers
+        start_response(status, headers)
+        txt = """
+        <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
 
-    txt = "<html><body><h1>hello</h1></body></html>"
-    return [txt.encode()]
+<body>
+ <h1>Це моя форма</h1>
+ <form action="http://localhost:15555/processform?a=b" method="POST">
+     <input type="text" name="usertext">
+     <input type="submit" value="Відправити">
+ </form>
+
+
+</body>
+
+</html>"""
+        return [txt.encode()]
+    elif pth == "/processform":
+
+        headers = [('Content-type', 'text/plain')]  # HTTP Headers
+        start_response(status, headers)
+        qs = environ['QUERY_STRING']
+        cl = environ.get('CONTENT_LENGTH', '')
+        get_data_dict = urllib.parse.parse_qs(qs)
+
+
+        try:
+            cl = int(cl)
+        except:
+            cl = 0
+
+        post_data = environ['wsgi.input'].read(cl)
+
+
+        txt = "This is the data:\n"
+        txt += str(get_data_dict) + "\n"
+        txt += str(post_data)
+
+        return [txt.encode()]
+    else:
+        headers = [('Content-type', 'text/plain')]  # HTTP Headers
+        start_response(status, headers)
+        return ["UNKNOWN REQUEST".encode()]
+
 
 ####################
 
