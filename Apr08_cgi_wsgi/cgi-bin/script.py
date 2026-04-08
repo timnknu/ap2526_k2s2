31727603df27eb364670b7d25247c26ef9a0a8c1
Your Name <you@example.com>
2026-04-08 11:16:03 +0300

    v1 - CGI GET

--- Apr08_cgi_wsgi/cgi-bin/script.py
+++ Apr08_cgi_wsgi/cgi-bin/script.py
@@ -1,5 +1,18 @@
 #!/bin/env python
+
+import os
+import urllib
+import urllib.parse
+
+
 print("Content-type: text/plain")
 print()
 
 print("This is the form result")
+qs = os.environ.get('QUERY_STRING', '')
+cl = os.environ.get('CONTENT_LENGTH', '')
+print(qs)
+print(cl)
+
+par_dict = urllib.parse.parse_qs(qs)
+print("DICT:", par_dict)
