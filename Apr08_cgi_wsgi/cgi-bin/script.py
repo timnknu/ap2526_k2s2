c004fb24ce11a2c7c23fe4857efa6a09fa94660d
Your Name <you@example.com>
2026-04-08 11:25:51 +0300

    v1 - CGI POST

--- Apr08_cgi_wsgi/cgi-bin/script.py
+++ Apr08_cgi_wsgi/cgi-bin/script.py
@@ -3,6 +3,7 @@
 import os
 import urllib
 import urllib.parse
+import sys
 
 
 print("Content-type: text/plain")
@@ -10,9 +11,15 @@ print()
 
 print("This is the form result")
 qs = os.environ.get('QUERY_STRING', '')
-cl = os.environ.get('CONTENT_LENGTH', '')
+cl = os.environ.get('CONTENT_LENGTH', '0')
 print(qs)
 print(cl)
+try:
+    cl = int(cl)
+except:
+    cl = 0
 
-par_dict = urllib.parse.parse_qs(qs)
+post_data = sys.stdin.read(cl)
+print('POST DATA:', )
+par_dict = urllib.parse.parse_qs(post_data)
 print("DICT:", par_dict)
